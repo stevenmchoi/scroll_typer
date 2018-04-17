e3024e5fdc4171b5c024927ede89038322e35080
Steven Choi <steven.m.choi@gmail.com>
2018-04-17 11:02:11 -0700

    Adjust text field w/ vertical lines

--- js/components/text_field.js
+++ js/components/text_field.js
@@ -1,10 +1,19 @@
 export const text_field = (ctx) => {
 	ctx.beginPath();
 	ctx.lineWidth = 1.0;
-	ctx.strokeRect(450, 300, 750, 125);
-	(ctx.font = '100px Roboto Mono'), 'monospace';
+	ctx.strokeRect(480, 300, 750, 125);
 	ctx.fillStyle = '#000';
 	ctx.fill();
-	ctx.fillText('Hello World!', 460, 400);
 	ctx.closePath();
+
+	// (ctx.font = '100px Roboto Mono'), '100px Courier', 'monospace';
+	ctx.fillText('Hello World!', 480, 400);
+
+	for (let location = 480; location < 1200; location += 60) {
+		ctx.beginPath();
+		ctx.moveTo(location, 0);
+		ctx.lineTo(location, 425);
+		ctx.strokeStyle = '#ccc';
+		ctx.stroke();
+	}
 };
