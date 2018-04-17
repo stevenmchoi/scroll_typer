b3bed15d0dfd5c94ad73a8845de7f358cc664469
Steven Choi <steven.m.choi@gmail.com>
2018-04-16 17:49:34 -0700

    Fix font issues and imported Roboto Mono successfully

--- js/entry.js
+++ js/entry.js
@@ -1,9 +1,15 @@
-var canvas = document.getElementById('myCanvas');
-var ctx = canvas.getContext('2d');
+document.addEventListener('DOMContentLoaded', () => {
+	const canvas = document.getElementById('myCanvas');
+	const ctx = canvas.getContext('2d');
 
-ctx.beginPath();
-ctx.lineWidth = 1.0;
-ctx.strokeRect(450, 300, 750, 125);
-ctx.fillStyle = '#eee';
-ctx.fill();
-ctx.closePath();
+	ctx.beginPath();
+	ctx.lineWidth = 1.0;
+	ctx.strokeRect(450, 300, 750, 125);
+	ctx.font = '100px Roboto Mono';
+	ctx.fillStyle = '#000';
+	ctx.fill();
+	ctx.fillText('Hello World!', 460, 400);
+	ctx.closePath();
+
+	// keyboard_img = new Image();
+});
