7d0f916116f5f70243f43a4924fb54392ac62d1f
Steven Choi <steven.m.choi@gmail.com>
2018-04-18 10:17:17 -0700

    Create bkgd canvas and rename game canvas

--- js/root.js
+++ js/root.js
@@ -2,7 +2,7 @@ import { text_field } from './components/text_field';
 import { keyboard } from './components/keyboard';
 
 export const root = () => {
-	const canvas = document.getElementById('myCanvas');
+	const canvas = document.getElementById('game-layer');
 	const ctx = canvas.getContext('2d');
 
 	text_field(ctx);
