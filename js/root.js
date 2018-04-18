067ca2e7b130d7d48d682b630cef89f10fb9b7bf
Steven Choi <steven.m.choi@gmail.com>
2018-04-18 13:04:36 -0700

    Begin game by clicking on canvas; removes click handler immediately

--- js/root.js
+++ js/root.js
@@ -1,11 +1,22 @@
 import { text_field } from './components/text_field';
 import { keyboard } from './components/keyboard';
 
-export const root = () => {
-	const canvas = document.getElementById('game-layer');
-	const ctx = canvas.getContext('2d');
+let canvas;
+let ctx;
+
+function refresh() {
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
 
 	text_field(ctx);
 
 	keyboard(ctx);
+
+	canvas.removeEventListener('click', refresh, true);
+}
+
+export const root = () => {
+	canvas = document.getElementById('game-layer');
+	ctx = canvas.getContext('2d');
+
+	canvas.addEventListener('click', refresh, true);
 };
