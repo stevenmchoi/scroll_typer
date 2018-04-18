e930ef175109cec9eb698d7409314b46f125ee19
Steven Choi <steven.m.choi@gmail.com>
2018-04-17 22:29:43 -0700

    Render text box in separate function

--- js/components/text_field.js
+++ js/components/text_field.js
@@ -2,23 +2,36 @@ import { dict } from '../dicts/dict';
 import { common_more_than_2 } from '../dicts/filtered';
 import { handleTyping } from '../actions/type_handling';
 
+function render_text_box(ctx, start_location) {
+	for (let location = start_location; location < 1200; location += 60) {
+		ctx.beginPath();
+		ctx.moveTo(location, 0);
+		ctx.lineTo(location, 425);
+		ctx.strokeStyle = '#ccc';
+		ctx.stroke();
+	}
+
+	ctx.strokeStyle = 'black';
+	ctx.strokeRect(start_location, 300.5, 1200, 125);
+	ctx.fillStyle = '#000';
+	ctx.fill();
+}
+
 export const text_field = (ctx) => {
 	const rand_common_word = () => {
 		return common_more_than_2[
 			Math.floor(Math.random() * common_more_than_2.length)
 		];
 	};
-	const start_location = 480;
+
+	const start_location = 480 + 0.5;
+
+	render_text_box(ctx, start_location);
 
 	document.addEventListener('keypress', (event) => {
-		ctx.clearRect(start_location, 0, 1200, 425);
+		ctx.clearRect(start_location, 0, 1200, 425 + 0.5);
 
-		ctx.beginPath();
-		ctx.lineWidth = 1.0;
-		ctx.strokeRect(start_location, 300, 1200, 125);
-		ctx.fillStyle = '#000';
-		ctx.fill();
-		ctx.closePath();
+		render_text_box(ctx, start_location);
 
 		let rand_word = rand_common_word();
 
@@ -26,15 +39,7 @@ export const text_field = (ctx) => {
 		ctx.fillText(rand_word, start_location, 400);
 
 		handleTyping(event, rand_word, rand_common_word);
-
-		for (let location = start_location; location < 1200; location += 60) {
-			ctx.beginPath();
-			ctx.moveTo(location, 0);
-			ctx.lineTo(location, 425);
-			ctx.strokeStyle = '#ccc';
-			ctx.stroke();
-		}
 	});
 
-	return rand_word;
+	// return rand_word;
 };
