e1fea40056c9e65897da2222aec8779345ba1f7f
Steven Choi <steven.m.choi@gmail.com>
2018-04-17 17:49:19 -0700

    Import sample dictionary

--- js/components/text_field.js
+++ js/components/text_field.js
@@ -1,15 +1,19 @@
+import { dict } from '../dicts/dict';
+
 export const text_field = (ctx) => {
+	const start_location = 480;
+
 	ctx.beginPath();
 	ctx.lineWidth = 1.0;
-	ctx.strokeRect(480, 300, 750, 125);
+	ctx.strokeRect(start_location, 300, 1200, 125);
 	ctx.fillStyle = '#000';
 	ctx.fill();
 	ctx.closePath();
 
-	// (ctx.font = '100px Roboto Mono'), '100px Courier', 'monospace';
-	ctx.fillText('Hello World!', 480, 400);
+	(ctx.font = '100px Roboto Mono'), '100px Courier', 'monospace';
+	ctx.fillText(dict[0], start_location, 400);
 
-	for (let location = 480; location < 1200; location += 60) {
+	for (let location = start_location; location < 1200; location += 60) {
 		ctx.beginPath();
 		ctx.moveTo(location, 0);
 		ctx.lineTo(location, 425);
