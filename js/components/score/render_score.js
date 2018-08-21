f98ed3927b83d771d2a63f2426a9d104faa62e7e
Steven Choi <steven.m.choi@gmail.com>
2018-08-20 23:43:19 -0700

    Render user score

--- js/components/score/render_score.js
+++ js/components/score/render_score.js
@@ -2,4 +2,18 @@ export default function renderScore() {
     if (!window.userScore) {
         window.userScore = 0;
     }
+
+    // // Button box
+    // ctx.strokeStyle = "black";
+    // ctx.strokeRect(400.5, 300.5, 170, 100);
+    // ctx.fillStyle = "#000";
+    // ctx.fill();
+
+    // Button text
+    ctx.font = "50px Roboto Mono";
+    ctx.textAlign = "right";
+    ctx.fillText("Score:", 1100.5, 700.5);
+    ctx.textAlign = "right";
+    ctx.fillText(userScore, 1100.5, 750.5);
+    ctx.textAlign = "start";
 }
