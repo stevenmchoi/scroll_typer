cb575ed13c7d33ea2878c440919441f174a38bac
Steven Choi <steven.m.choi@gmail.com>
2018-08-22 00:35:56 -0700

    Separate Listener callbacks from inButtonListener

--- js/actions/in_button_listener.js
+++ js/actions/in_button_listener.js
@@ -10,18 +10,21 @@ function inButton([button_x, button_y, button_len, button_height], event) {
 	);
 }
 
-export default function inButtonListener(dimens, btn_fn) {
-	canvas.addEventListener("mousemove", event => {
-		if (inButton(dimens, event)) {
-			document.body.style.cursor = "pointer";
-		} else {
-			document.body.style.cursor = "default";
-		}
-	});
+function buttonMousemoveListener(event) {
+	if (inButton(dimens, event)) {
+		document.body.style.cursor = "pointer";
+	} else {
+		document.body.style.cursor = "default";
+	}
+}
 
-	canvas.addEventListener("click", event => {
-		if (inButton(dimens, event)) {
-			btn_fn();
-		}
-	});
+function buttonClickListener(event) {
+	if (inButton(dimens, event)) {
+		btn_fn();
+	}
+}
+
+export default function inButtonListener(dimens, btn_fn) {
+	canvas.addEventListener("mousemove", buttonMousemoveListener);
+	canvas.addEventListener("click", buttonClickListener);
 }
