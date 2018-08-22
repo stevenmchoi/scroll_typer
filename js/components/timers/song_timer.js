5bbce13e98235867022c5aca0b44efe33a5ba9e9
Steven Choi <steven.m.choi@gmail.com>
2018-08-22 04:02:53 -0700

    Render song timer

--- js/components/timers/song_timer.js
+++ js/components/timers/song_timer.js
@@ -1,6 +1,9 @@
 export default function songTimer() {
     window.songTimerInterval = setInterval(() => {
-        console.log("song1.currentTime: " + song1.currentTime);
-        console.log("song1.duration: " + song1.duration);
-    }, 1000);
+        ctx.clearRect(0, 700, 300, canvas.height);
+
+        ctx.font = "50px Roboto Mono";
+        ctx.fillText("Timer:", 100.5, 700.5);
+        ctx.fillText((Math.floor((song1.duration - song1.currentTime) * 10) / 10.0).toFixed(1), 100.5, 750.5);
+    }, 1);
 }
