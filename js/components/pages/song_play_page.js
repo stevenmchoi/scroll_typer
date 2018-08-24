79407cc885c4f913b0a4665f102373bbf934184f
Steven Choi <steven.m.choi@gmail.com>
2018-08-23 18:09:07 -0700

    Rebundle and comment out play/pause button

--- js/components/pages/song_play_page.js
+++ js/components/pages/song_play_page.js
@@ -22,27 +22,27 @@ export default function songPlayPage() {
 
 	window.currentSong.play();
 
-	// Play/pause button
+	// TODO: Play/pause button
 	// renderButton([190.5, 320.5, 80, 80], ["", 200.5, 373.5], playPauseButton);
-	currentSong.addEventListener("playing", playPauseButton);
+	// currentSong.addEventListener("playing", playPauseButton);
 
-	console.log(window.currentSong.playing);
+	// console.log(window.currentSong.playing);
 
-	ctx.fillStyle = "black";
-	ctx.strokeRect(190.5, 320.5, 80, 80);
-	if (window.currentSong.playing) {
-		ctx.fillRect(208.5, 335.5, 15, 50);
-		ctx.fillRect(238.5, 335.5, 15, 50);
-	} else {
-		ctx.strokeStyle = 'black';
-		ctx.beginPath();
-		ctx.moveTo(208.5, 335.5);
-		ctx.lineTo(238.5, 360.5);
-		ctx.lineTo(208.5, 385.5);
-		ctx.lineTo(208.5, 335.5);
-		ctx.stroke();
-		ctx.fill();
-	}
+	// ctx.fillStyle = "black";
+	// ctx.strokeRect(190.5, 320.5, 80, 80);
+	// if (window.currentSong.playing) {
+	// 	ctx.fillRect(208.5, 335.5, 15, 50);
+	// 	ctx.fillRect(238.5, 335.5, 15, 50);
+	// } else {
+	// 	ctx.strokeStyle = 'black';
+	// 	ctx.beginPath();
+	// 	ctx.moveTo(208.5, 335.5);
+	// 	ctx.lineTo(238.5, 360.5);
+	// 	ctx.lineTo(208.5, 385.5);
+	// 	ctx.lineTo(208.5, 335.5);
+	// 	ctx.stroke();
+	// 	ctx.fill();
+	// }
 
 	setupWordField();
 
